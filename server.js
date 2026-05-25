1c5fa666338fae4a5fd209ab62a039b3a975b74c
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2026-05-25 23:09:26 +0600

    Update server listening message to reflect correct port

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-const http = require("http")
+const http = require ("http")
 
 const server=http.createServer((req,res)=>{
     res.end("Welcome to Todo App")
 })
 
 server.listen(5000,"127.0.0.1",()=>{
-    console.log("Server is listening port 3000")
+    console.log("Server is listening port 5000")
 })
+
+/** 
+* /todos - Get - All Todo
+* /todos/create-todos POST Create
+*/
